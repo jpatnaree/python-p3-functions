d44588e48fd0618d05a52fbb51c0d4b936ee9592
Patnaree Johnson <j.patnaree.t@gmail.com>
2023-11-11 17:11:50 -0500

    lab

--- lib/functions.py
+++ lib/functions.py
@@ -1,16 +1,40 @@
 #!/usr/bin/env python3
 
 def greet_programmer():
-    pass
+    print("Hello, programmer!")
 
 def greet(name):
-    pass
+    print(f"Hello, {name}!")
 
 def greet_with_default(name="programmer"):
-    pass
+    print (f"Hello, {name}!")
+
+greet_with_default("Guido")
 
 def add(num1, num2):
-    pass
+    return num1 + num2
 
 def halve(number):
-    pass
+    return number / 2
+
+# def my_function(param):
+#     print("Running my_function")
+#     return param + 1
+
+# my_function_return_value = my_function(1)
+
+# my_function_return_value
+
+# def say_hi(name = "friend"):
+#     print(f"Hi there, {name}!")
+
+# say_hi()
+# say_hi("BooBoo")
+
+def stylish_painter():
+    best_hairstyle = "Bob Ross"
+    return "Jean-Michel Basquiat"
+    return best_hairstyle
+    print(best_hairstyle)
+
+print(stylish_painter())
